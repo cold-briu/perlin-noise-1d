b0c30cf54e2615872a48edfe1ed7204474f77e1b
Andrés Fernández <andresavena19@gmail.com>
2023-01-24 09:52:05 -0500

    fet: basic bounce

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use nannou::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    nannou::sketch(view).run()
+}
+
+fn view(app: &App, frame: Frame) {
+    // Prepare to draw.
+    let draw = app.draw();
+    let boundary = app.window_rect();
+    let sine = app.time.sin();
+    let x = map_range(sine, -1.0, 1.0, boundary.left(), boundary.right());
+
+    draw.background().color(rgb(1.0, 1.0, 1.0));
+    draw.ellipse().color(rgb(0.2, 0.2, 0.2)).x_y(x, 0.0);
+
+    // render the draw
+    draw.to_frame(app, &frame).unwrap();
 }
