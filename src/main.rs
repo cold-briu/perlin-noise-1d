ef2a5c14f17e14ea10dd2a11a10eaa999826dba8
Andrés Fernández <andresavena19@gmail.com>
2023-01-24 19:01:38 -0500

    feat: basic static 1d perlin

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,7 @@
 use nannou::{
-    noise::{NoiseFn, Perlin},
+    noise::{BasicMulti, NoiseFn, Perlin},
     prelude::*,
 };
-
-static NOISE_STEP: f32 = 300.;
-// static CANVAS_SIZE: [f32; 2] = [400., 400.];
-
-// struct Model {
-//     noise: BasicMulti,
-//     points: Vec<Vec2>,
-//     frame_start: u64,
-//     xoff: i32,
-// }
-
-// fn model(app: &App) -> Model {
-//     let _window = app
-//         .new_window()
-//         .size(CANVAS_SIZE[0], CANVAS_SIZE[1])
-//         .view(view)
-//         .build()
-//         .unwrap();
-
-//     Model {
-//         noise: BasicMulti::new(),
-//         points: vec![],
-//         frame_start: 0,
-//     }
-// }
 
 fn main() {
     // nannou::app(model).update(update).run()
@@ -35,31 +10,43 @@ fn main() {
 
 // fn view(app: &App, model: &Model, frame: Frame) {
 fn view(app: &App, frame: Frame) {
-    let light_color = rgb(0.2, 0.2, 0.2);
+    let light_color = rgb(0.8, 0.8, 0.8);
+    let mid_color = rgb(0.4, 0.4, 0.4);
+    let dark_color = rgb(0.1, 0.1, 0.1);
+
     let boundary = app.window_rect();
+    let full_w = boundary.w() as i32;
+    let full_h = boundary.h() as i32;
+
+    let resolution = 6;
+    let cols = full_w / resolution;
+    let rows = full_h / resolution;
 
     let draw = app.draw();
-    draw.background().color(rgb(1.0, 1.0, 1.0));
+    draw.background().color(dark_color);
 
-    // let current_step = (app.elapsed_frames()) as f32 / NOISE_STEP;
-    // let _x = Perlin::new().get([current_step.into(), 0.]);
-    // let x = map_range(_x, -1.0, 1.0, boundary.left(), boundary.right());
+    // let noise = Perlin::new();
+    let noise = BasicMulti::new();
+    let mut points: Vec<Vec2> = vec![];
 
-    // draw.ellipse().w_h(10., 10.).color(light_color).x_y(x, 10.);
+    for x in 0..cols {
+        for y in 0..rows {
+            let pos_x = map_range(x * resolution, 0, cols, boundary.left(), boundary.right());
+            let pos_y = map_range(y * resolution, 0, rows, boundary.top(), boundary.bottom());
 
-    // draw.line()
-    //     .start(Vec2::new(boundary.left(), 0.))
-    //     .end(Vec2::new(boundary.right(), 0.))
-    //     .color(light_color);
+            draw.ellipse()
+                .color(mid_color)
+                .x_y(pos_x, pos_y)
+                .w_h(2.0, 2.0);
 
-    let points = (0..boundary.w() as i32).map(|i| {
-        let x = i as f32 - 25.0; //subtract 25 to center the sine wave
-        let point = pt2(x, x.sin()) * 20.0; //scale sine wave by 20.0
-        point
-    });
+            let p = noise.get([pos_x as f64, 0.]);
+            let noise_y = map_range(p, -1.0, 1.0, boundary.top(), boundary.bottom());
+            points.push(pt2(pos_x, noise_y as f32));
+        }
+    }
 
     draw.polyline()
-        .x(0 as f32)
+        .x(boundary.left())
         .weight(1.0)
         .points(points)
         .color(light_color);
@@ -67,5 +54,3 @@ fn view(app: &App, frame: Frame) {
     // render the draw
     draw.to_frame(app, &frame).unwrap();
 }
-
-// fn update(app: &App, model: &mut Model, _update: Update) {}
